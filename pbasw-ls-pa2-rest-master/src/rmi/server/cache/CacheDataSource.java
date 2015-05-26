c532e1107ee518b0f239c6e5760ac5f48680fbb7
martinrasmussen <martinrasmussen92@gmail.com>
2015-05-26 21:01:30 +0200

    Deleted Project

--- pbasw-ls-pa2-rest-master/src/rmi/server/cache/CacheDataSource.java
+++
@@ -1,8 +0,0 @@
-package rmi.server.cache;
-
-import java.util.Map;
-
-public interface CacheDataSource<K,V> {
-    V get(K key);
-    Map<K,V> getAll();
-}
--- pbasw-ls-pa2-rest-master/src/rmi/server/cache/ReplacementPolicy.java
+++
@@ -1,8 +0,0 @@
-package rmi.server.cache;
-
-public interface ReplacementPolicy<T> {
-    T itemToReplace();
-    void registerAccess(T cacheKey);
-    void remove(T cacheKey);
-    void clear();
-}
